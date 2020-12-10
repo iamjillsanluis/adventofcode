59de1985e604338584e045c3e8124cc0abc1f308
Jill San Luis <jill.devs@gmail.com>
2020-12-09 21:33:52 -0500

    day 9 puzzle part 2

--- 2020/9/main.py
+++ 2020/9/main.py
@@ -31,6 +31,22 @@ def find_corrupted_entry(data, pre_amble_size):
     return None
 
 
+def find_contiguous_range_min_max_sum(data, target_sum):
+    index_of_target = data.index(target_sum)
+    for start_index in range(0, index_of_target):
+        running_sum = 0
+        running_index = start_index
+        while running_sum < target_sum and running_index < index_of_target:
+            running_sum += data[running_index]
+            running_index += 1
+
+        if running_sum == target_sum:
+            sorted_range = sorted(data[start_index:running_index])
+            return sorted_range[0] + sorted_range[-1]
+
+    return []
+
+
 def test1():
     assert 127 == find_corrupted_entry(data_feed("short_input.txt"), 5)
 
@@ -40,5 +56,22 @@ def part1():
     print("Part 1: corrupted entry:", find_corrupted_entry(data_feed("input.txt"), 25))
 
 
+def test2():
+    assert 62 == find_contiguous_range_min_max_sum(data_feed("short_input.txt"), 127)
+
+
+def part2():
+    test2()
+    data = data_feed("input.txt")
+    print(
+        "Part 2: sum of min-max",
+        find_contiguous_range_min_max_sum(
+            data,
+            target_sum=find_corrupted_entry(data, 25)
+        )
+    )
+
+
 if __name__ == "__main__":
     part1()
+    part2()
