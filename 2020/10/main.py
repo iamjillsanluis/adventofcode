8c8560400381a9155e3e32c1a1283f812a1813ff
Jill San Luis <jill.devs@gmail.com>
2020-12-10 11:51:34 -0500

    day 10 puzzle, extract joltage difference resolution from counting differences to make figuring out distinct arrangements in part 2 easier.

--- 2020/10/main.py
+++ 2020/10/main.py
@@ -1,4 +1,4 @@
-def parse_adapter_joltage_ratings(file):
+def parse_available_adapters(file):
     data = []
     with open(file) as fh:
         for line in fh:
@@ -6,38 +6,44 @@ def parse_adapter_joltage_ratings(file):
             if line:
                 data.append(int(line))
 
-    return data
+    sorted_adapters = sorted(data)
+    sorted_adapters.append(sorted_adapters[-1] + 3)  # device built-in adapter rating
+    return sorted_adapters
+
+
+def joltage_differences(adapters):
+    current_joltage = 0
+
+    differences = []
+    for adapter in adapters:
+        differences.append(adapter - current_joltage)
+        current_joltage = adapter
+
+    return differences
 
 
 def product_of_joltage_differences(adapters):
-    sorted_adapters = sorted(adapters)
-    sorted_adapters.append(sorted_adapters[-1] + 3)  # device built-in adapter rating
-
-    current_joltage = 0
     total_1_joltage, total_3_joltage = 0, 0
 
-    for adapter in sorted_adapters:
-        joltage_difference = adapter - current_joltage
-        if joltage_difference == 1:
+    for difference in joltage_differences(adapters):
+        if difference == 1:
             total_1_joltage += 1
-        elif joltage_difference == 3:
+        elif difference == 3:
             total_3_joltage += 1
-
-        current_joltage = adapter
 
     return total_1_joltage * total_3_joltage
 
 
 def test1():
-    assert 35 == product_of_joltage_differences(parse_adapter_joltage_ratings("short_input.txt"))
-    assert 220 == product_of_joltage_differences(parse_adapter_joltage_ratings("short_input2.txt"))
+    assert 35 == product_of_joltage_differences(parse_available_adapters("short_input.txt"))
+    assert 220 == product_of_joltage_differences(parse_available_adapters("short_input2.txt"))
 
 
 def part1():
     test1()
     print(
         "Part 1: joltage extreme product",
-        product_of_joltage_differences(parse_adapter_joltage_ratings("input.txt"))
+        product_of_joltage_differences(parse_available_adapters("input.txt"))
     )
 
 
