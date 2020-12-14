c1347e5a77dea7a88e602dc8b1fff445f4e75c07
Jill San Luis <jill.devs@gmail.com>
2020-12-13 23:54:35 -0500

    day 13 puzzle part 2.

--- 2020/13/main.py
+++ 2020/13/main.py
@@ -7,24 +7,26 @@ def parse_input(file):
 
         optimal_departure_time = int(lines[0])
         bus_ids = [
-            int(bus_id)
+            bus_id
             for bus_id in lines[1].split(",")
-            if bus_id != "x"
         ]
 
         return optimal_departure_time, bus_ids
 
 
+def buses_departures(bus_ids, optimal_departure_time):
+    return [
+        (bus_id, int(optimal_departure_time / bus_id), optimal_departure_time % bus_id)
+        for bus_id in [int(b) for b in bus_ids if b != "x"]
+    ]
+
+
 def earliest_bus_solution(file):
     optimal_departure_time, bus_ids = parse_input(file)
-    buses_departures = [
-        (bus_id, int(optimal_departure_time / bus_id), optimal_departure_time % bus_id)
-        for bus_id in bus_ids
-    ]
 
     wait_time = sys.maxsize
     bus = None
-    for bus_id, bus_round, mins_before_departure_time in buses_departures:
+    for bus_id, bus_round, mins_before_departure_time in buses_departures(bus_ids, optimal_departure_time):
         if mins_before_departure_time == 0:
             return 0
         else:
@@ -48,5 +50,56 @@ def part1():
     print("Part 1: solution", earliest_bus_solution("input.txt"))
 
 
+def earliest_departure_timestamp(bus_schedules, start_timestamp=None):
+    bus_departure_offsets = {
+        int(bus_id): offset
+        for offset, bus_id in enumerate(bus_schedules)
+        if bus_id != "x"
+    }
+
+    reference_bus = max(bus_id for bus_id in bus_departure_offsets)
+    reference_bus_offset = bus_departure_offsets[reference_bus]
+
+    constraints = [
+        (bus_id, offset - reference_bus_offset)
+        for bus_id, offset in bus_departure_offsets.items()
+        if bus_id != reference_bus
+    ]
+
+    iteration = int((start_timestamp or reference_bus) / reference_bus)
+    while True:
+        reference_bus_timestamp = reference_bus * iteration
+        found = True
+        for bus_id, offset in constraints:
+            if (reference_bus_timestamp + offset) % bus_id > 0:
+                found = False
+                break
+
+        if found:
+            print(f"{iteration} timestamp: {reference_bus_timestamp}")
+            return reference_bus_timestamp - reference_bus_offset
+
+        if iteration % 1000000 == 0:
+            print(f"checkpoint: {iteration} timestamp: {reference_bus_timestamp}")
+
+        iteration += 1
+
+
+def test2():
+    assert 3417 == earliest_departure_timestamp([17, "x", 13, 19])
+    assert 754018 == earliest_departure_timestamp([67, 7, 59, 61])
+    assert 779210 == earliest_departure_timestamp([67, "x", 7, 59, 61])
+    assert 1261476 == earliest_departure_timestamp([67, 7, "x", 59, 61])
+    assert 1202161486 == earliest_departure_timestamp([1789, 37, 47, 1889])
+    assert 1068781 == earliest_departure_timestamp([7, 13, "x", "x", 59, "x", 31, 19])
+
+
+def part2():
+    test2()
+    _, bus_ids = parse_input("input.txt")
+    print("Part 2: solution", earliest_departure_timestamp(bus_ids, start_timestamp=100000000000000))
+
+
 if __name__ == "__main__":
     part1()
+    part2()
